4050c40f56463d19aafecf998b38802eb7e86756
Jsweeley <jsweeleyhd@gmail.com>
2015-12-09 11:19:58 -0800

    JOpiton convert

--- Rock Papers Scissors/src/RockPaperScissors.java
+++ Rock Papers Scissors/src/RockPaperScissors.java
@@ -1,11 +1,12 @@
 //Credit to Simon Forsberg
 import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 import java.util.Random;
 public class RockPaperScissors
 
 //Test 123
-//Dean's Test
-//Zach's Test
 
 {
 	public static void main (String[] args) 
@@ -16,17 +17,17 @@ public class RockPaperScissors
 		String loop="yes"; // Starts our loop
 		Scanner input = new Scanner(System.in); // Creates scanner object
 		Random rgen = new Random();
-		System.out.println("Hello, for this exercise we're going to be playing everyone's favourite game, Rock-Paper-Scissors!");
+		JOptionPane.showMessageDialog(null, "Hello, for this exercise we're going to be playing everyone's favourite game, Rock-Paper-Scissors!");
 
 		while (loop.equals("yes")) // This loop keeps our game going only while our string.loop is equal to 'yes'
 		{ 
 			cChoice=rgen.nextInt(3)+1;
-			System.out.println("Please make your selection: R - Rock. P - Paper. S - Scissors");
+			JOptionPane.showInputDialog("Please make your selection: R - Rock. P - Paper. S - Scissors");
 			String hInput = input.nextLine();  // variable for players choice (R/P/S) 
 			String hChoice = hInput.toUpperCase(); //Converts to Upper case in case user did not
 			if (hChoice.equals("R") || hChoice.equals("P") || hChoice.equals("S")) // Ensures player has entered the correct choice for the game to continue
 			{
-				System.out.println("");
+				JOptionPane.showMessageDialog(null, "");
 
 				if (hChoice.equals("R"))  // This converts pChoice to the numeric value for "Rock"
 				{
@@ -43,78 +44,44 @@ public class RockPaperScissors
 
 				if (pChoice == cChoice) // Takes care of Ties
 				{ 
-					System.out.println("Tie Game!");
-					System.out.println("");
-					tie++;
-					rounds++;
+					JOptionPane.showMessageDialog(null, "Tie Game!" + "\n" + "" + "\n" + tie++ + "\n" + rounds++);
 				} else      // Accounts for scoring for non-tie scenarios
 				{
 					if (cChoice==1 && pChoice==3) // Computer picks Rock and player has Scissors - adds point to score and rounds
 					{
-						System.out.println("Computer picked Rock!");
-						System.out.println("Rock beats Scissors!");
-						System.out.println("**Computer Wins!**");
-						System.out.println("");
-						cScore++;
-						rounds++;
+						JOptionPane.showMessageDialog(null, "Computer picked Rock!" + "\n" + "Rock beats Scissors!" + "\n" + "**Computer Wins!**" + "\n" + "" + "\n" + cScore++ + "\n" + rounds++);
 					} 
 
 					if (cChoice==1 && pChoice==2) // Computer picks Rock and player has Paper - adds point to score and rounds
 					{
-						System.out.println("Computer picked Rock!");
-						System.out.println("Paper beats Rock!");
-						System.out.println("**Player Wins!**");
-						System.out.println("");
+						JOptionPane.showMessageDialog(null, "Computer picked Rock!" + "\n" + "Paper beats Rock!" + "\n" + "**Player Wins!**" + "\n" + "");
 						pScore++;
 						rounds++;
 					} 
 
 					if (cChoice==2 && pChoice==3) // Computer picks Paper and player has Scissors - adds point to score and rounds
 					{
-						System.out.println("Computer picked Paper!");
-						System.out.println("Scissors beats Paper!");
-						System.out.println("**Player Wins!**");
-						System.out.println("");
-						pScore++;
-						rounds++;
+						JOptionPane.showMessageDialog(null, "Computer picked Paper!" + "\n" + "Scissors beats Paper!" + "\n" + "**Player Wins!**" + "\n" + "" + "\n" + pScore++ + "\n" + rounds++);
 					} 
 
 					if (cChoice==2 && pChoice==1) // Computer picks Paper and player has Rock - adds point to score and rounds
 					{
-						System.out.println("Computer picked Paper!");
-						System.out.println("Paper beats Rock!");
-						System.out.println("**Computer Wins!**");
-						System.out.println("");
-						cScore++;
-						rounds++;
+						JOptionPane.showMessageDialog(null, "Computer picked Paper!" + "\n" + "Paper beats Rock!" + "\n" + "**Computer Wins!**" + "\n" + "" + "\n" + cScore++ + "\n" + rounds++);
 					} 
 
 					if (cChoice==3 && pChoice==1)  // Computer picks Scissors and player has Rock - adds point to score and rounds
 					{
-						System.out.println("Computer picked Scissors!");
-						System.out.println("Rock beats Scissors!");
-						System.out.println("**Player Wins!**");
-						System.out.println("");
-						pScore++;
-						rounds++;
+						JOptionPane.showMessageDialog(null, "Computer picked Scissors!" + "\n" + "Rock beats Scissors!" + "\n" + "**Player Wins!**" + "\n" + "" + "\n" + pScore++ + "\n" + rounds++);
 					} 
 
 					if (cChoice==3 && pChoice==2) // Computer picks Scissors and player has Paper - adds point to score and rounds
 					{
-						System.out.println("Computer picked Scissors!");
-						System.out.println("Scissors beats Paper!");
-						System.out.println("**Computer Wins!**");
-						System.out.println("");
-						cScore++;
-						rounds++;
+						JOptionPane.showMessageDialog(null, "Computer picked Scissors!" + "\n" + "Scissors beats Paper!" + "\n" + "**Computer Wins!**" + "\n" + "" + "\n" + cScore++ + "\n" + rounds++);
 					} 
 				}                            
 			} else  // end the game
 			{
-				System.out.println ("Sorry, you didn't pick Rock, Paper, or Scissors. The game will end now.");
-				System.out.println ("Here are the final scores after " + rounds +" rounds:");
-				System.out.println ("You\tComputer\tTies");
-				System.out.println (" "+ pScore +"\t   " + cScore + "\t\t " + tie);
+				JOptionPane.showMessageDialog(null, "Sorry, you didn't pick Rock, Paper, or Scissors. The game will end now." + "\n" + "Here are the final scores after " + rounds +" rounds:" + "\n" + "You\tComputer\tTies" + "\n" + " "+ pScore +"\t   " + cScore + "\t\t " + tie);
 				loop = "no"; // terminates the while loop keeping the game going
 			}
 		}  
